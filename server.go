d7aa631612cca97e2af783079e2ee7f3cda0f450
EricRiney <fails.to.install@gmail.com>
2016-05-31 06:27:40 +0000

    hsjsgg

--- server.go
+++ server.go
@@ -8,18 +8,15 @@ import (
   "github.com/julienschmidt/httprouter"
 )
 
-
-
 func main() {
   fmt.Println("Starting App")
 
-  //load wiki titles into memory
+  // load wiki titles into memory
 
   router := httprouter.New()
   router.GET("/goapi", responseHandler)
 	log.Fatal(http.ListenAndServe(":9761", router))
 }
-
 
 func responseHandler(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
   fmt.Println("hittttt")
