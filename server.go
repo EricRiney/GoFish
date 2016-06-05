023a68ca3b61302209c4c61911f35fc5b3639e6f
EricRiney <fails.to.install@gmail.com>
2016-06-05 22:50:42 +0000

    meow

--- server.go
+++ server.go
@@ -6,11 +6,13 @@ import (
 	"net/http"
   "fmt"
   "github.com/julienschmidt/httprouter"
+  "encoding/json"
+  "strconv"
 )
 
 func main() {
   fmt.Println("Starting App")
-  
+
   // load wiki titles into memory
 
   router := httprouter.New()
@@ -19,9 +21,23 @@ func main() {
 }
 
 func responseHandler(w http.ResponseWriter, req *http.Request, param httprouter.Params) {
-  fmt.Println(param.ByName("query"))
-  
-  
+  //fmt.Println(param.ByName("query"))
+  // make slice
+  var letters [13]string
+  // for loop slice
+  for i := 0; i < 13; i++ {
+	   letters[i] = (   "" + param.ByName("query") + strconv.Itoa(i)   )
+	}
+
+  // populate slice with 13 entries of the query + counter
+  // like: query0, query1, etc....
+  // return that
+
+
+
+  jsonArray, _ := json.Marshal(letters)
+  w.Write(jsonArray)
+  return
   //w.Write([]byte("A response"))
   //io.WriteString(w, "Hello world!")
 }
